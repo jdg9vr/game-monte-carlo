243bcee410fdb1f314e6888bca9d10f4615fd7e4
Josh Gen <jdg9vr@virginia.edu>
2022-07-14 10:19:19 -0400

    Added setup.py file

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import setup
+
+setup(name='game-monte-carlo',
+      version='0.1',
+      description='Monte Carlo Game',
+      url='https://github.com/jdg9vr/game-monte-carlo',
+      author='Josh Gen',
+      author_email='jdg9vr@virginia.edu',
+      license='MIT',
+      packages=['game-monte-carlo'])
