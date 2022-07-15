1dae1ea15142f9f9059e33ccbe1e968e1232f37e
Josh Gen <64161160+jdg9vr@users.noreply.github.com>
2022-07-15 10:10:57 -0400

    Add docstring to init

--- game_monte_carlo/__init__.py
+++ game_monte_carlo/__init__.py
@@ -1,3 +1,6 @@
+"""
+This file initializes the package when pip install is run. It preloads the Die, Game, and Analyzer functions and prints package loaded so the user knows the init file was run.
+"""
 from .montecarlo import Die
 from .montecarlo import Game
 from .montecarlo import Analyzer
